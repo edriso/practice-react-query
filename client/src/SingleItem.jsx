788b7ca32e65a5cdcde4487b12174bdf60a5b90e
Mohamed Idris <edriso.dev@gmail.com>
2026-01-21 17:22:09 +0200

    implement task status toggle with React Query mutation and error handling

--- client/src/SingleItem.jsx
+++ client/src/SingleItem.jsx
@@ -1,10 +1,35 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { axiosInstance } from './utils';
+import { toast } from 'react-toastify';
+
 const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient();
+
+  const { isPending, mutate: toggleTaskStatus } = useMutation({
+    mutationFn: ({ taskId, taskIsDone }) =>
+      axiosInstance.patch(`/${taskId}`, { isDone: !taskIsDone }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks'],
+      });
+    },
+    onError: (error) => {
+      toast.error(error.response?.data || error.message);
+    },
+  });
+
   return (
     <div className='single-item'>
       <input
         type='checkbox'
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={() =>
+          toggleTaskStatus({
+            taskId: item.id,
+            taskIsDone: item.isDone,
+          })
+        }
+        disabled={isPending}
       />
       <p
         style={{
